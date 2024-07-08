e846282a2a4fddea9f859ca0752497a150c78d2f
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-07-08 13:37:16 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -5,7 +5,7 @@ class House:
 
     def go_to(self, new_floor):
         for floor in range(1, new_floor + 1):
-            if new_floor == 1 or new_floor <= self.number_of_floors:
+            if new_floor >= 1 and new_floor <= self.number_of_floors:
                 print(floor)
             else:
                 print('Такого этажа не существует')
@@ -15,7 +15,7 @@ class House:
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
-h2.go_to(10)
+h2.go_to(-1)
 
 
 
